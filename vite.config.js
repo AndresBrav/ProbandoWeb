a34c7da0cc56a22100daa96c4a47cbfadf98e682
Andres Bravo <bravoandres1706@gmail.com>
2025-02-21 16:43:47 -0400

    actualizacion2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/ProbandoWeb/",
+  base: "/ProbandoWeb",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,  // 👈 Asegura que las rutas funcionen en desarrollo
+  }
 })
