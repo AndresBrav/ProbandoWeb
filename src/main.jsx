501cee45a4d3548c414968ea2f31eaad49011e95
Andres Bravo <bravoandres1706@gmail.com>
2025-02-21 17:03:46 -0400

    funcionaPerfectoHashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Link, Route, Routes } from 'react-router'
+import { BrowserRouter, HashRouter, Link, Route, Routes } from 'react-router'
 import Hola from './Hola.jsx'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/ProbandoWeb">
+  <HashRouter basename="/ProbandoWeb">
     <StrictMode>
       <Routes>
         <Route path='/' element={<App />}></Route>
         <Route path='/hola' element={<Hola />}></Route>
       </Routes>
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
